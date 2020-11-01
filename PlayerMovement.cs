7ac795ae64f61d43b0794f7b7d62aa3b5b7ec572
coolkoopa <54568101+coolkoopa@users.noreply.github.com>
2020-11-01 11:28:33 -0800

    Add files via upload

---
+++ PlayerMovement.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController controller;
+    public float speed;
+    public Animator animator;
+    public Transform cam;
+    public float turnSmooth = 0.1f;
+    float smoothV;
+    float gravity = -9.8f;
+    Vector3 velocity;
+    public Transform groundCheck;
+    float groundDist = 0.4f;
+    public LayerMask groundMask;
+    bool isGrounded = false;
+    void Update()
+    {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDist, groundMask);
+
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
+        float x = Input.GetAxisRaw("Horizontal");
+        float z = Input.GetAxisRaw("Vertical");
+
+        Vector3 move = new Vector3(x, 0f, z).normalized;
+
+        if (move.magnitude >= .1f)
+        {
+            float targetAngle = Mathf.Atan2(move.x, move.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref smoothV, turnSmooth);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+            Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+
+            controller.Move(moveDir.normalized * speed * Time.deltaTime);
+        }
+
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * speed * Time.deltaTime);
+
+        if (z != 0 || x != 0)
+        {
+            animator.SetBool("isRunning", true);
+        }
+        else
+        {
+            animator.SetBool("isRunning", false);
+        }
+    }
+}
+
